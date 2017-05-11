ba95fc4b1d8e68b1a258f9133a4d99fdd6c18b32
git <gypsyk911@gmail.com>
2017-05-11 10:08:03 +0700

    Added one migration. TODO: abs_class, othr_migr

---
+++ migrations/m170511_020708_create_quiz_tables.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m170511_020708_create_quiz_tables extends Migration
+{
+    public function up()
+    {
+        $this->createTable('quiz_question', [
+            'id' => $this->primaryKey(),
+            'test_id' => $this->integer()->notNull()->comment('Test id'),
+            'type' => $this->integer()->notNull()->comment('Question type'),
+            'question' => $this->text()->notNull()->comment('Question text'),
+            'answers' => 'JSON' . ' DEFAULT NULL COMMENT "Variants of answers"',
+            'r_answers' => 'JSON' . ' NOT NULL COMMENT "Right answer(-s)"'
+        ]);
+
+        $this->addCommentOnTable('quiz_question', 'List of tests questions');
+
+        //Foreign key for test table
+        $this->createIndex('idx-quiz_question-test_id', 'quiz_question', 'test_id');
+        $this->addForeignKey('fk-quiz_question-test_id', 'quiz_question', 'test_id', 'quiz_test', 'id', 'CASCADE', 'CASCADE');
+
+        //Foreign key for question-type table
+        $this->createIndex('idx-quiz_question-type', 'quiz_question', 'type');
+        $this->addForeignKey('fk-quiz_question-type', 'quiz_question', 'type', 'quiz_question_type', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    public function down()
+    {
+        $this->dropForeignKey('fk-quiz_question-test_id', 'quiz_question');
+        $this->dropIndex('idx-quiz_question-test_id', 'quiz_question');
+
+        $this->dropForeignKey('fk-quiz_question-type', 'quiz_question');
+        $this->dropIndex('idx-quiz_question-type', 'quiz_question');
+
+        $this->dropTable('quiz_question');
+    }
+}
